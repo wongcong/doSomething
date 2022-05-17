df5e6c5e0bb1019c5ec85a6146a48088cf568ca0
wrblack <wrblack@sina.cn>
2022-05-17 09:37:40 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  onLaunch() {
+    console.log("App Launch");
+  },
+  onShow() {
+    console.log("App Show");
+  },
+  onHide() {
+    console.log("App Hide");
+  },
+});
+</script>
+<style lang="scss">
+@import "tailwindcss/base";
+@import "tailwindcss/utilities";
+</style>
